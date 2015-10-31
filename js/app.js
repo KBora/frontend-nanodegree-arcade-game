52b896391246ab5b83ab709b44dab97a2e04156f
Kian Woo <kian.woo@gmail.com>
2015-11-01 07:00:45 +1100

    Create Player class outline

--- js/app.js
+++ js/app.js
@@ -24,7 +24,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+    // TODO: set sprite value
+};
+Player.prototype.update = function(dt) {
+    // TO DO: update position
+}
+Player.prototype.render = function() {
+    // TO DO: draw player on screen
+}
+Player.prototype.handleInput = function() {
+    // TO DO: handles user input?
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
